7ea5b49a85e2d2694df0c16cf77b86e1c8e01265
meruyertkopbayeva <meruyert.kopbayeva@nu.edu.kz>
2023-02-25 21:19:14 +0600

    lab6

---
+++ main.js
@@ -0,0 +1,20 @@
+// 2
+function perimeter(a,b) {
+    console.log('perimeter is',2 * (a+b))
+}
+perimeter(2,3)
+// 13
+function greatesAmongTwo(a,b) {
+    if (a > b) {
+        return a
+    } else {
+        return b
+    }
+}
+console.log('greatest number is:',greatesAmongTwo(2,4))
+// 3
+function areaOfCircle(r) {
+    console.log('area of circle:',r*r*3.14)
+}
+
+areaOfCircle(2)
